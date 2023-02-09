75b4b7e64bdbf08dc6202b2bee671f7983c129fd
Yin Zijie <3174465976@qq.com>
2023-02-09 14:20:33 +0800

    添加作者头像和广告的逻辑

--- src/pages/api/articleInfo.ts
+++ src/pages/api/articleInfo.ts
@@ -5,7 +5,7 @@ import { IArticleProps } from '../article/[articleId]';
 
 const getArticleInfoData = (req: NextApiRequest, res: NextApiResponse<IArticleProps>): void => {
   const { articleId } = req.query;
-  axios.get(`${CMSDOMAIN}/api/articles/${articleId}`).then(result => {
+  axios.get(`${CMSDOMAIN}/api/articles/${articleId}?populate=*`).then(result => {
     const data = result.data.data.attributes || {};
     res.status(200).json(data);
   });
